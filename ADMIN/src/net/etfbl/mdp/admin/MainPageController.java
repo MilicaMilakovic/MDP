0c957b6fedf67f8fcb0a846a7665e76aa0bc10cc
Milica <milakovicmilica@yahoo.com>
2021-07-27 15:28:15 +0200

    Admin - prijem obavjestenja

--- ADMIN/src/net/etfbl/mdp/admin/MainPageController.java
+++ ADMIN/src/net/etfbl/mdp/admin/MainPageController.java
@@ -1,14 +1,22 @@
 package net.etfbl.mdp.admin;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class MainPageController implements Initializable {
 	
@@ -17,10 +25,15 @@ public class MainPageController implements Initializable {
 	@FXML
 	public AnchorPane contentArea;
 	
+	@FXML
+	public TextArea notificationField;
+	@FXML
+	public ImageView notificationBell;
+	
 	 @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		 
-		 
+		 checkForNotifications();
 	}
 	 
 	public void users() {
@@ -52,6 +65,70 @@ public class MainPageController implements Initializable {
 	}
 	
 	public void schedule() {
+		Parent root = null;
+		try {
+						
+			root = FXMLLoader.load(getClass().getResource("Schedule.fxml"));
+			contentArea.getChildren().removeAll();
+			contentArea.getChildren().setAll(root);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void shutDown() {
 		
+		System.exit(0);
+	}
+	
+	
+	public void checkForNotifications() {
+		
+		new Thread(()-> {
+			MulticastSocket socket = null;
+			byte[] buffer = new byte[256];
+			
+			try {
+				socket = new MulticastSocket(20000);
+				InetAddress group = InetAddress.getByName("230.0.0.0");
+				socket.joinGroup(group);
+				
+				while(true) {
+					DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+					socket.receive(packet);
+					
+					String notification = new String(packet.getData(),0, packet.getLength());
+					
+					notificationField.appendText(notification+"\n");
+					
+					ringTheBell();
+					
+					try {
+						Thread.sleep(500);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+				
+			} catch (IOException e) {				
+				e.printStackTrace();
+			}
+		}).start();
+	}
+	
+	
+	private void ringTheBell() {
+		
+		RotateTransition rotateTransition=new RotateTransition(Duration.millis(200), notificationBell);
+		rotateTransition.setFromAngle(30);
+		rotateTransition.setToAngle(-30);
+		
+		rotateTransition.setAutoReverse(true);
+		rotateTransition.setCycleCount(3);
+		rotateTransition.setInterpolator(Interpolator.EASE_BOTH);
+		rotateTransition.play();
+		rotateTransition.setOnFinished(e-> {notificationBell.setRotate(0);});		
 	}
 }
---
+++ ADMIN/src/net/etfbl/mdp/admin/ScheduleController.java
@@ -0,0 +1,16 @@
+package net.etfbl.mdp.admin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ScheduleController implements Initializable {
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
